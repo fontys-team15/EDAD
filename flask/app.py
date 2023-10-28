ee74fa1a228fd0ce83fa4f7dd0100037c5cbda7e
Genchuwu <evgenikurtev@abv.bg>
2023-10-28 15:45:39 +0200

    clean

--- flask/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-@app.route("/<username>")
-def index(username = None):
-    return render_template("index.html", username = username)
