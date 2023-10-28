be47ae8143f350524db5b182426f373ca81ba8b1
IHATEROCKS <105079798+Genchuwu@users.noreply.github.com>
2023-10-28 16:31:02 +0200

    RESTful API (#9)

--- app.py
+++ app.py
@@ -1,78 +1,101 @@
-from flask import (Flask, render_template, redirect, request, session, g)
+#!/usr/bin/env python
 import os
-import random
-import bcrypt
+import time
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+import jwt
+from werkzeug.security import generate_password_hash, check_password_hash
 
+# initialization
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-app.secret_key = os.urandom(24)
-
-
-class User:
-    def __init__(self, username, password, id):
-        self.username = username
-        self.password = password
-        self.id = id
-
-    def __repr__(self):
-        return f'<{self.username}:{self.password}>'
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
 
 
-users_list = []
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
 
-id = 1
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-users_list.append( # add test user
-    User(
-        username="gg",
-        password=b'$2b$12$0lFcDSbmXJQGSHj6rrdaGOL9v04wRxqMsvFkoMZNGc97MVY7z5DiK', # pass = 123
-        id=id)
-)
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
+    def generate_auth_token(self, expires_in=600):
+        return jwt.encode(
+            {'id': self.id, 'exp': time.time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
 
-
-@app.before_request
-def before_request():
-    g.user = None
-    if 'user_id' in session:
-        user = [x for x in users_list if x.id == session['user_id']][0]
-        g.user = user
-
-
-@app.route('/')
-def main_page():
-    return render_template("index.html")
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    session.pop('user_id', None)
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        for user in users_list:
-            if user.username == username:
-                user_password = password.encode('utf-8')
-                hash = user.password
-                result = bcrypt.checkpw(user_password, hash)
-                if result:
-                    session['user_id'] = user.id
-                    return redirect('/dashboard')
-        return redirect('/login')
-    return render_template('login.html')
+    @staticmethod
+    def verify_auth_token(token):
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'],
+                              algorithms=['HS256'])
+        except:
+            return
+        return User.query.get(data['id'])
 
 
-@app.route('/dashboard')
-def dashboard():
-    if not g.user:
-        return redirect('/login')
-    return render_template('dashboard.html', username=g.user.username)
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
 
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect("/")
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)    # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
+
+
+if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
+    app.run(debug=True)
