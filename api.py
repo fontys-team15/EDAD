5426271072b494200f089864e1d2376b8fd5dc42
Martin Dobrev <md.martindobrev@protonmail.com>
2023-11-09 11:13:16 +0100

    change state machine

--- api.py
+++ api.py
@@ -116,7 +116,7 @@ def get_resource():
         r = requests.post("https://pb0w7r2ew5.execute-api.eu-central-1.amazonaws.com/1/step", json=json.dumps({
             "input": "{}",
             "name": g.user.email,
-            "stateMachineArn": "arn:aws:states:eu-central-1:657026912035:stateMachine:MyStateMachine-11pw20640"
+            "stateMachineArn": "arn:aws:states:eu-central-1:657026912035:stateMachine:CreditCardWorkflow"
     }))
     except:
         return r
