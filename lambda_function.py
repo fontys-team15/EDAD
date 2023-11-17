14699045009e312a91617cf143a928c191cc2217
Martin Dobrev <md.martindobrev@protonmail.com>
2023-11-17 12:30:30 +0100

    lambda func

--- lambda_function.py
+++ lambda_function.py
@@ -2,65 +2,48 @@ import json
 import boto3
 from jinja2 import Template
 
+BUCKET = 'user-configurations'
+INSTANCE_ID = 'i-0ea9bbf03dbaf01dd'
+
+s3 = boto3.client('s3')
 
 def read_template_from_file():
-    s3 = boto3.client('s3')
-    s3_obj = s3.get_object(Bucket='user-configurations', Key='templates/args.tfvars.j2')
-    args_template = s3_obj["Body"].read()
+    s3_obj = s3.get_object(Bucket=BUCKET, Key='templates/cloud-formation.jinja2')
+    template = s3_obj["Body"].read()
     s3_obj["Body"].close()
-
-    return args_template.decode("utf-8")
+    return template.decode("utf-8")
 
 def render_template(template_content, name, vpc_cidr, subnet_cidrs, brokers, broker_volume_size):
     template = Template(template_content)
     rendered_content = template.render(name=name, vpc_cidr=vpc_cidr, subnet_cidrs=subnet_cidrs, brokers=brokers, broker_volume_size=broker_volume_size)
     return rendered_content
 
+def exec_template(rendered_template, email):
+    cloudformation_client = boto3.client('cloudformation')
+
+    response = cloudformation_client.create_stack(
+        StackName=f"{email}",
+        TemplateBody=rendered_template,
+        Capabilities=['CAPABILITY_IAM']
+    )
+    return 200
+
 def lambda_handler(event, context):
-    name = json_data["name"]
-    vpc_cidr = json_data["vpc_cidr"]
-    subnet_cidrs = json_data["subnet_cidrs"]
-    brokers = json_data["brokers"]
-    broker_volume_size = json_data["broker_volume_size"]
+    print(event)
+    email = event["email"]
+    name = event["cluster_name"]
+    vpc_cidr = event["vpc_cidr"]
+    subnet_cidrs = event["subnet_cidrs"]
+    print(subnet_cidrs)
+    brokers = event["brokers"]
+    broker_volume_size = event["broker_volume_size"]
 
     template_content = read_template_from_file()
+    rendered_template = render_template(template_content, name, vpc_cidr, subnet_cidrs, brokers, broker_volume_size)
+    print(rendered_template)
 
-    rendered_tf_args = render_template(template_content, name, vpc_cidr, subnet_cidrs, brokers, broker_volume_size)
+    exec_template(rendered_template, email)
 
-    return rendered_tf_args.encode("utf-8")
-
-def lambda_handler(event, context):
-    instance_id = 'i-0ea9bbf03dbaf01dd'
-    args = event
-    print(args)
-    script = f'echo -e {args} > /home/ssm-user/args.tfvars'
-
-    ssm = boto3.client('ssm')
-
-    params = {
-        'InstanceIds': [instance_id],
-        'DocumentName': 'AWS-RunShellScript',
-        'Parameters': {
-            'commands': [script]
+    return {
+        'status': 200
         }
-    }
-
-    try:
-        response = ssm.send_command(**params)
-        command_id = response['Command']['CommandId']
-        print(f'Command sent to EC2 instance. Command ID: {command_id}')
-        return {
-            'statusCode': 200,
-            'body': 'Command sent successfully!'
-        }
-    except Exception as e:
-        print(f'Error sending command to EC2 instance: {e}')
-        return {
-            'statusCode': 500,
-            'body': 'Error sending command to EC2 instance.'
-        }
-
-
-# print(lambda_handler().encode("utf-8"))
-
-print(write_to_node())
