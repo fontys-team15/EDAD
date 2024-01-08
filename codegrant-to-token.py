566efb1703451a5cb322f4fb1b78827288eedc7a
Martin Dobrev <md.martindobrev@protonmail.com>
2024-01-09 00:59:30 +0200

    fix: exchange works
    Now you can exchange a code grant for an id_token

--- codegrant-to-token.py
+++ codegrant-to-token.py
@@ -1,13 +1,4 @@
 import requests
-import jsonify
-#import boto3
-
-
-'''
-1. receives request with code grant (should be in the event body)
-2. exchanges the code for a token by hitting the oauth2 endpoint
-3. pass the token to the next lambda
-'''
 
 ENDPOINT = "https://auth.sigmadad.fitness/oauth2/token"
 HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -16,20 +7,19 @@ def exchange_for_token(cgrant: str):
     data = {
         'grant_type': 'authorization_code',
         'client_id': '7islnjaa6l6ch9ughu2v1v7cj7',
-        'code': f'cgrant',
+        'code': cgrant,
         'redirect_uri': 'https://www.google.com',
         'scope': 'auth/msk'
     }
-    r = request.get(ENDPOINT, headers=HEADERS, data=data) 
-    print(r)
-    return TOKEN
+    r = requests.post(ENDPOINT, headers=HEADERS, data=data) 
+    return r.json() 
 
 def lambda_handler(event, context):
-    cgrant = event[]
-    token = exchange_for_token()
-
-    return jsonify({
+    cgrant = event
+    token = exchange_for_token(cgrant)
+    return {
         'status': 200,
-        'token': token 
-    })
+        'token': token["id_token"]
+    }
 
+print(lambda_handler(input(),3))
