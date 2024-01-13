e52c82f3d72b4fe1ef793c92f5c0321f113d598b
Martin Dobrev <md.martindobrev@protonmail.com>
2024-01-13 16:03:01 +0100

    fix: data dict in global namespace

--- codegrant-to-token.py
+++ codegrant-to-token.py
@@ -1,16 +1,18 @@
+#!/bin/python3
 import requests
 
 ENDPOINT = "https://auth.sigmadad.fitness/oauth2/token"
 HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
+data = {
+    'grant_type': 'authorization_code',
+    'client_id': '7islnjaa6l6ch9ughu2v1v7cj7',
+    'code': "",
+    'redirect_uri': 'https://www.google.com',
+    'scope': 'auth/msk'
+}
 
 def exchange_for_token(cgrant: str):
-    data = {
-        'grant_type': 'authorization_code',
-        'client_id': '7islnjaa6l6ch9ughu2v1v7cj7',
-        'code': cgrant,
-        'redirect_uri': 'https://www.google.com',
-        'scope': 'auth/msk'
-    }
+    data["code"] = cgrant
     r = requests.post(ENDPOINT, headers=HEADERS, data=data) 
     return r.json() 
 
